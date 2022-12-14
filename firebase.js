0ceff93ea90b3e6c64cfdd397ed2228c8a151bef
Navin Kumar <111299104+Navinksinhaa@users.noreply.github.com>
2022-12-14 19:41:36 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCZmRSB3MSPM6mSAUrCdsq1HfDAX_o_odM",
-    authDomain: "whatsapp-mern-3c982.firebaseapp.com",
-    projectId: "whatsapp-mern-3c982",
-    storageBucket: "whatsapp-mern-3c982.appspot.com",
-    messagingSenderId: "713257076069",
-    appId: "1:713257076069:web:57a0da3ca618849df08d8c"
-  };
